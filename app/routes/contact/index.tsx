a496f5b63a1059a782bb559d25c82a08518bb54c
iSalah-YT <82966610+iSalah-YT@users.noreply.github.com>
2025-10-03 18:47:00 +0100

    frontend complete

---
+++ app/routes/contact/index.tsx
@@ -0,0 +1,117 @@
+import type { Route } from './+types';
+
+// export async function action({ request }: Route.ActionArgs) {
+//   const formData = await request.formData();
+//   const name = formData.get('name') as string;
+//   const email = formData.get('email') as string;
+//   const subject = formData.get('subject') as string;
+//   const message = formData.get('message') as string;
+
+//   const errors: Record<string, string> = {};
+
+//   if (!name) errors.name = 'Name is required';
+//   if (!email) {
+//     errors.email = 'Email is required';
+//   } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+//     errors.email = 'Invalid email format';
+//   }
+//   if (!subject) errors.subject = 'Subject is required';
+//   if (!message) errors.message = 'Message is required';
+
+//   if (Object.keys(errors).length > 0) {
+//     return { errors };
+//   }
+
+//   const data = { name, email, subject, message };
+
+//   return { message: 'Form submitted successfully', data };
+// }
+
+const ContactPage = ({ actionData }: Route.ComponentProps) => {
+  return (
+    <div className="max-w-3xl mx-auto mt12 px-6 py-8 bg-gray-900">
+      <h2 className="text-3xl font-bold text-white mb-8 text-center">
+        📬 Contact Me
+      </h2>
+
+      <form
+        action="https://formspree.io/f/mgvnybwa"
+        method="post"
+        className="space-y-6"
+      >
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Full Name
+          </label>
+
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full mt-1 px-4 p-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Email
+          </label>
+
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full mt-1 px-4 p-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="subject"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Subject
+          </label>
+
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            className="w-full mt-1 px-4 p-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Message
+          </label>
+
+          <textarea
+            id="message"
+            name="message"
+            rows={4}
+            className="w-full mt-1 px-4 p-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          ></textarea>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition cursor-pointer"
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactPage;
